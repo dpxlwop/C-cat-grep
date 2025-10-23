7ea11ca1ad6f66c419da4ef46a4d11df9944eb63
Proclus Harmund <procluha@ev-p4.kzn.21-school.ru>
2025-10-23 13:45:47 +0300

    fixed that shit

--- src/cat/cat.h
+++ src/cat/cat.h
@@ -1,6 +1,5 @@
-void normalize_flags(int *flags, int count, int *deleted_flags);
-int file_proccess(const char *filename, int *flags);
-void print_with_line_numbers(const char *string, int *line_number);
-void print_with_line_numbers_non_empty(const char *string, int *line_number);
-void print_dollar_at_end();
-void replace_tab(char *string);
+int normalize_flags(int* flags, int count);
+int file_proccess(const char* filename, int* flags, int flag_count);
+void print_with_line_numbers(const char* string, int* line_number);
+void print_with_line_numbers_non_empty(const char* string, int* line_number);
+void replace_tab(char* string);
